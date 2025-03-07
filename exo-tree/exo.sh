586d85a5b448655d1128d106f730dd795376a48a
Charlotte <smit-charlotte@outlook.com>
2025-03-07 11:02:04 +0100

    save

---
+++ exo-tree/exo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p public/{css/{footer,header,main},img,js}
+touch public/js/main.js
+touch public/css/{footer,header,main}/style.css
+touch public/img/img{1..2}.png
+touch public/index.html
